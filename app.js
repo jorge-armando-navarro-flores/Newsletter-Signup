a603d2bd18ec461f5775658e5488ccae29e7bdd2
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-08-09 11:29:48 -0500

    env

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 
-
-
 app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
-})
+});
 
 app.post("/", (req, res) => {
   const firstName = req.body.firstName;
@@ -25,24 +23,23 @@ app.post("/", (req, res) => {
         status: "subscribed",
         merge_fields: {
           FNAME: firstName,
-          LNAME: lastName
-        }
-      }
-    ]
+          LNAME: lastName,
+        },
+      },
+    ],
   };
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us14.api.mailchimp.com/3.0/lists/f1ad13facc"
+  const url = "https://us14.api.mailchimp.com/3.0/lists/f1ad13facc";
 
   const options = {
     method: "POST",
-    auth: "Janf77:26e4ec28d1c9a06550549c0e38cb6199-us14"
-  }
+    auth: process.env.MAILCHIMP_API_KEY,
+  };
 
   const request = https.request(url, options, (response) => {
-
-    if(response.statusCode === 200) {
+    if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
     } else {
       res.sendFile(__dirname + "/failure.html");
@@ -50,19 +47,19 @@ app.post("/", (req, res) => {
 
     response.on("data", (data) => {
       console.log(JSON.parse(data));
-    })
-  })
+    });
+  });
 
   request.write(jsonData);
   request.end();
-
-
-})
+});
 
 app.post("/failure", (req, res) => {
-  res.redirect("/")
-})
+  res.redirect("/");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000.");
-})
+});
+
+// https://git.heroku.com/infinite-bastion-97530.git
